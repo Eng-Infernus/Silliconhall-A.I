1870ce28a16008e58e8903cb6fa5b2772651cf90
Ahmed Saeed Rafaat <59177115+Eng-Infernus@users.noreply.github.com>
2024-01-28 01:22:06 +0300

    Update linear regression.py

--- linear_regression/linear regression.py
+++ linear_regression/linear regression.py
@@ -14,7 +14,6 @@ data = pd.read_csv("student-mat.csv", sep=";")
 predict = "G3"
 
 data = data[["G1", "G2", "absences","failures", "studytime","G3"]]
-data = shuffle(data) # Optional - shuffle the data
 
 x = np.array(data.drop([predict], 1))
 y =np.array(data[predict])
